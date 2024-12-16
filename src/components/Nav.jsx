e81e57fcf15a80bfa7f1e4b5bf7e792aea3ab456
siva102002 <siva16102002@gmail.com>
2024-12-16 11:50:29 +0530

    modify the css file in nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,4 @@
-import styles from "src/components/nav.module.css";
+import styles from "./nav.module.css";
 export default function Nav() {
   return <div className={styles.nav}>🍕 Food App</div>;
 }
